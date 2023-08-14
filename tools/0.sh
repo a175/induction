801ecdea461baeae6573bd447a519931e08b8876
NU <a175@users.noreply.github.com>
2023-08-14 12:23:06 +0900

    html

--- tools/0.sh
+++ tools/0.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cat p.tex |\
+
+MAINTEX=$1
+HEADER=$2
+FOOTER=$3
+cat $MAINTEX |\
     awk '/newest/{A=1} {if(A==1){print $0}}'|\
     awk '/end{prop}/{A=1} {if(A!=1){print $0}}'|\
     awk ' {if(A==1){print $0}} /label/{A=1}'|\
-    cat htmlbase/index.0.html - htmlbase/index.1.html
+    cat $HEADER - $FOOTER
 
