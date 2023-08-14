8d2a6c93e64c0a28640f2bc776bd107a5c012dd1
NU <a175@users.noreply.github.com>
2023-08-14 12:32:11 +0900

    bug fix

--- tools/0.sh
+++ tools/0.sh
@@ -7,5 +7,9 @@ cat $MAINTEX |\
     awk '/newest/{A=1} {if(A==1){print $0}}'|\
     awk '/end{prop}/{A=1} {if(A!=1){print $0}}'|\
     awk ' {if(A==1){print $0}} /label/{A=1}'|\
+    sed 's/$/\\\(/'|sed 's/$/\\\)/'|\
+    sed 's/$/\\\(/'|sed 's/$/\\\)/'|\
+    sed 's/$/\\\(/'|sed 's/$/\\\)/'|\
+    sed 's/$/\\\(/'|sed 's/$/\\\)/'|\
     cat $HEADER - $FOOTER
 
